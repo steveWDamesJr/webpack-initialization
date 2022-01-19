a61ea3d2a50f39969e82624adcaaf714709f4d4c
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-01-19 15:14:57 -0500

    Add Webpack
    -Add bundles | Templates | css loaders | dev start server
    -Add images
    -Modify: Webpack config | package.json | dist / src directory structures

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
 import _ from 'lodash';
+import './style.css';
+import Github from './popup-github.png';
+import House from './cap-background.jpg';
+import Data from './data.xml';
+import Notes from './data.csv';
+// No warning
+//import data from './data.json';
+// Warning shown, this is not allowed by the spec.
+//import { foo } from './data.json';
+
+function printMe() {
+  console.log('I get called from print.js!');
+}
 
 function component() {
   const element = document.createElement('div');
-
+  const btn = document.createElement('button');
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myGitIcon = new Image();
+  myGitIcon.src = Github;
+  const capHouse = new Image();
+  capHouse.src = House;
+
+
+  element.appendChild(myGitIcon);
+  element.appendChild(capHouse);
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,49 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
   },
-};
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './src/index.html'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+    ],
+  },
+ };
